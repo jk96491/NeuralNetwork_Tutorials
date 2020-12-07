8700194a206072b398514b8efa0844719ad1bd66
RaptorJung <jk96491@naver.com>
2020-12-07 11:03:10 +0900

    학습 방법 종류 추가

--- Week03/Tensorflow/Model.py
+++ Week03/Tensorflow/Model.py
@@ -1,15 +1,44 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
+from Utils import SuffleData
 
 
 class MyModel(Model):
     def __init__(self):
         super(MyModel, self).__init__()
         weight_init = tf.keras.initializers.RandomNormal()
-        self.model = tf.keras.Sequential()
-        self.model.add(Dense(3, use_bias=True, kernel_initializer=weight_init))
+        self.model = tf.keras.Sequential(Dense(1, use_bias=True, kernel_initializer=weight_init, input_shape=(3,)))
 
-    def call(self, x):
-        x = self.model(x)
-        return x
+        self.opt = tf.optimizers.Adam(learning_rate=0.01)
+        self.model.compile(optimizer=self.opt, loss=self.mse_error)
+        self.build(input_shape=(None, 3))
+
+        print(self.model.summary())
+
+    def call(self, inputs, training=None, mask=None):
+        predict = self.model(inputs)
+        return predict
+
+    def train(self, x_train, y_train, batch_size, epoch, use_fit):
+        if use_fit:
+            self.model.fit(x_train, y_train, batch_size=batch_size, epochs=epoch)
+        else:
+            for ep in range(epoch):
+                for i in range(int(len(x_train) / batch_size)):
+                    x, y = SuffleData(x_train, y_train, batch_size)
+                    with tf.GradientTape() as Tape:
+                        predict = self.call(x)
+                        loss = self.mse_error(predict, y)
+
+                    gradients = Tape.gradient(loss, self.trainable_variables)
+                    self.opt.apply_gradients(zip(gradients, self.trainable_variables))
+                    if (i + 1) % 100 == 0:
+                        print('Epoch {:4d}/{} step{:4d} loss: {:.6f}'.format(ep + 1, epoch, i + 1, loss.numpy()))
+
+    def eval(self, x_test, y_test):
+        self.model.evaluate(x_test, y_test)
+
+    def mse_error(self, predict, answer):
+        loss = tf.reduce_mean(tf.square(predict - answer))
+        return loss
--- Week03/Tensorflow/multi.py
+++ Week03/Tensorflow/multi.py
@@ -1,22 +1,20 @@
-import tensorflow as tf
-from tensorflow.keras import optimizers as optim
 import random
 import numpy as np
-from Utils import SuffleData
 from Week03.Tensorflow.Model import MyModel
 
-input_size = 3
-output_size = 1
-hidden_size = 20
-hidden_dim = 128
-
 min_len = 50
-max_len = 1000
+max_len = 10000
 normalize = 1
 
+use_fit = False
+
+train_data_len = 9000
 
 subject_score_list = []
 final_score_list = []
+
+batch_size = 10
+epochs = 5
 
 for i in range(max_len):
     kor = random.randrange(10, 100) * normalize
@@ -28,23 +26,17 @@ for i in range(max_len):
 
     final_score_list.append([final_score * normalize])
 
-x_train = np.asarray(subject_score_list)
-y_train = np.asarray(final_score_list)
+
+X_data = np.asarray(subject_score_list)
+Y_data = np.asarray(final_score_list)
+
+x_train = X_data[:train_data_len]
+y_train = Y_data[:train_data_len]
+
+x_test = X_data[train_data_len:]
+y_test = Y_data[train_data_len:]
 
 model = MyModel()
 
-optimizers = optim.Adam(learning_rate=0.001)
-
-nb_epochs = 15000
-for epoch in range(nb_epochs + 1):
-    rand = random.randrange(min_len, max_len)
-    x_train, y_train = SuffleData(x_train, y_train, rand)
-
-    with tf.GradientTape() as Tape:
-        prediction = model(x_train)
-        cost = tf.reduce_mean(tf.square(prediction - y_train))
-
-    gradients = Tape.gradient(cost, model.trainable_variables)
-    optimizers.apply_gradients(zip(gradients, model.trainable_variables))
-
-    print('Epoch {:4d}/{} Cost: {:.6f}'.format(epoch, nb_epochs, cost))
+model.train(x_train, y_train, batch_size, epochs, use_fit)
+model.eval(x_test, y_test)
